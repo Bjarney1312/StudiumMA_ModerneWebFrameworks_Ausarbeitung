9b1be6681eea393a7d1b257c8cbcaf416d6fc8f9
Ivonne Kneißig <kneissig.ivonne@fh-swf.de>
2024-01-25 21:35:39 +0100

    Recipe-details page (version 1)

--- src/app/components/recipe-details/recipe-details.component.ts
+++ src/app/components/recipe-details/recipe-details.component.ts
@@ -1,25 +1,45 @@
-import { Component, inject } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {Component, inject, OnInit} from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { RecipeService} from '../../services/recipe.service';
 import { Recipe } from '../../data/recipe';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatIcon} from "@angular/material/icon";
+import {NgForOf} from "@angular/common";
+import {MatTableModule} from '@angular/material/table';
+import {Ingredient} from "../../data/ingredient";
 @Component({
   selector: 'app-recipe-details',
   standalone: true,
-  imports: [],
+  imports: [
+    MatChipsModule,
+    MatIcon,
+    NgForOf,
+    MatTableModule
+  ],
   templateUrl: './recipe-details.component.html',
   styleUrl: './recipe-details.component.css'
 })
-export class RecipeDetailsComponent {
+export class RecipeDetailsComponent implements OnInit{
   route: ActivatedRoute = inject(ActivatedRoute);
-  recipeId = -1;
   recipeService = inject(RecipeService);
   recipe: Recipe | undefined;
 
+  dataSource: Ingredient[] = [];
+
+  displayedColumns: string[] = ['Menge', 'Zutat'];
+
   constructor() {
-    const recipeId = parseInt(this.route.snapshot.params['id'], 10);
-    this.recipeService.getRecipeById(recipeId).then(recipe => {
-      this.recipe = recipe;
-    });
+
   }
+
+  ngOnInit() {
+    this.loadRecipe();
+  }
+
+  async loadRecipe() {
+    const recipeId = parseInt(this.route.snapshot.params['id'], 10);
+    this.recipe = await this.recipeService.getRecipeById(recipeId);
+    this.dataSource = this.recipe?.ingredients ? this.recipe?.ingredients : [];
+  }
+
 }
